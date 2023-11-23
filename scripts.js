5610662c8132cc26410e17924a5d5a5ce2a47c72
villiger9 <ali.shaker93@gmail.com>
2023-11-23 15:00:10 +0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,96 @@
+// Weatherbit API key
+const apiKey = '86a4f707a6c34956b0b4831843f1676a';
+
+// Function to fetch weather data from Weatherbit API
+async function fetchData() {
+  const response = await fetch(`https://api.weatherbit.io/v2.0/forecast/hourly?lat=33.51&lon=36.27&key=${apiKey}`);
+  const data = await response.json();
+  return data.data;
+}
+
+// Function to create a line chart using D3.js
+async function createChart() {
+  const data = await fetchData();
+
+  const margin = { top: 20, right: 30, bottom: 50, left: 50 };
+  const width = 600 - margin.left - margin.right;
+  const height = 400 - margin.top - margin.bottom;
+
+  const container = d3.select('#chart-container');
+
+  const svg = container.append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform', `translate(${margin.left},${margin.top})`);
+
+  const times = data.map(entry => entry.timestamp_local);
+  const temperatures = data.map(entry => entry.temp);
+
+  const x = d3.scaleTime()
+    .domain([new Date(times[0]), new Date(times[times.length - 1])])
+    .range([0, width]);
+
+  const y = d3.scaleLinear()
+    .domain([d3.min(temperatures) - 1, d3.max(temperatures) + 1])
+    .range([height, 0]);
+
+  const line = d3.line()
+    .x(d => x(new Date(d.timestamp_local)))
+    .y(d => y(d.temp));
+
+  // Line
+  svg.append('path')
+    .data([data])
+    .attr('class', 'line')
+    .attr('d', line)
+    .style('stroke', '#3498db'); // Line color
+
+  // Data points
+  svg.selectAll('.dot')
+    .data(data)
+    .enter().append('circle')
+    .attr('class', 'dot')
+    .attr('cx', d => x(new Date(d.timestamp_local)))
+    .attr('cy', d => y(d.temp))
+    .attr('r', 5) // Radius of the data points
+    .style('fill', '#3498db'); // Data point color
+
+  // Add axes
+  svg.append('g')
+    .attr('transform', `translate(0,${height})`)
+    .call(d3.axisBottom(x).ticks(d3.timeHour.every(6)).tickFormat(d3.timeFormat('%m-%d %H:%M')))
+    .selectAll('text')
+    .attr('transform', 'rotate(-45)') // Rotate x-axis labels for better readability
+    .style('text-anchor', 'end');
+
+  svg.append('g')
+    .call(d3.axisLeft(y));
+
+  // Add gridlines
+  svg.append('g')
+    .attr('class', 'grid')
+    .attr('transform', `translate(0,${height})`)
+    .call(d3.axisBottom(x).ticks(d3.timeHour.every(6)).tickSize(-height).tickFormat(''));
+
+  svg.append('g')
+    .attr('class', 'grid')
+    .call(d3.axisLeft(y).tickSize(-width).tickFormat(''));
+
+  // Add labels
+  svg.append('text')
+    .attr('transform', `translate(${width / 2},${height + margin.top + 20})`)
+    .style('text-anchor', 'middle')
+    .text('Time');
+
+  svg.append('text')
+    .attr('transform', 'rotate(-90)')
+    .attr('y', 0 - margin.left)
+    .attr('x', 0 - (height / 2))
+    .attr('dy', '1em')
+    .style('text-anchor', 'middle')
+    .text('Temperature (°C)');
+}
+
+// Initialize the chart
+createChart();
