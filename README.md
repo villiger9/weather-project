a8cb93f90301ece8f964bb60501c6cd362058429
villiger9 <ali.shaker93@gmail.com>
2023-11-25 23:09:09 +0300

    added comments and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # weather-project
- 
+
+# Weather Data Visualization
+
+## Description
+
+This project uses D3.js to visualize hourly weather forecast data from the Weatherbit API. It displays a line chart showing the temperature changes over time.
+
+## Prerequisites
+
+- Web browser
+- Weatherbit API key (obtain one at [Weatherbit](https://www.weatherbit.io/api))
+
+## Installation
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/villiger9/weather-data-visualization.git
+   ```
--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ const apiKey = '86a4f707a6c34956b0b4831843f1676a';
 
 // Function to fetch weather data from Weatherbit API
 async function fetchData() {
+  // Fetch hourly forecast data from Weatherbit API
   const response = await fetch(`https://api.weatherbit.io/v2.0/forecast/hourly?lat=33.51&lon=36.27&key=${apiKey}`);
   const data = await response.json();
   return data.data;
@@ -12,21 +13,26 @@ async function fetchData() {
 async function createChart() {
   const data = await fetchData();
 
+  // Set up SVG container and dimensions
   const margin = { top: 20, right: 30, bottom: 50, left: 50 };
   const width = 600 - margin.left - margin.right;
   const height = 400 - margin.top - margin.bottom;
 
+  // Select the chart container
   const container = d3.select('#chart-container');
 
+  // Create an SVG element within the container
   const svg = container.append('svg')
     .attr('width', width + margin.left + margin.right)
     .attr('height', height + margin.top + margin.bottom)
     .append('g')
     .attr('transform', `translate(${margin.left},${margin.top})`);
 
+    // Extract timestamps and temperatures from the data
   const times = data.map(entry => entry.timestamp_local);
   const temperatures = data.map(entry => entry.temp);
 
+  // Set up scales for x and y axes
   const x = d3.scaleTime()
     .domain([new Date(times[0]), new Date(times[times.length - 1])])
     .range([0, width]);
@@ -35,18 +41,19 @@ async function createChart() {
     .domain([d3.min(temperatures) - 1, d3.max(temperatures) + 1])
     .range([height, 0]);
 
+    // Define a line function for the chart
   const line = d3.line()
     .x(d => x(new Date(d.timestamp_local)))
     .y(d => y(d.temp));
 
-  // Line
+  // Draw the line chart
   svg.append('path')
     .data([data])
     .attr('class', 'line')
     .attr('d', line)
     .style('stroke', '#3498db'); // Line color
 
-  // Data points
+  // Draw data points as circles
   svg.selectAll('.dot')
     .data(data)
     .enter().append('circle')
